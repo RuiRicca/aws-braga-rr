356d91387a5210104d5ce05422b2a2f9356b9785
RuiRicca <ruiricca@gmail.com>
2025-10-28 12:11:42 +0000

    Update baseDados.py

--- baseDados.py
+++ baseDados.py
@@ -4,4 +4,4 @@ server=sys.argv[1]
 user=sys.argv[2]
 password=sys.argv[3]
 
-print(f"Acesso à base de dados:\n Servidor: {server}\n Utilizador: {user}\n Pass: {password}")
+print(f"Acesso à base de dados:\n Servidor: {server}\n Utilizador: {user}\n Pass: #########")
